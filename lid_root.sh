f42651ed50f006c7d64d6834680bb7e443fafa1b
Дмитрий Карих <the.dr.hax@gmail.com>
2016-05-02 00:22:16 +0300

    lid_root.sh: Disable Wi-Fi if lid is closed for a long time

--- lid_root.sh
+++ lid_root.sh
@@ -4,6 +4,10 @@
 # change_cpu_governor: changes governor to powersave when lid is closed
 governor_control="true"
 
+# wifi_toggle: toggle wi-fi when lid state is changed
+wifi_control="true"
+wifi_control_delay="300" # seconds until wi-fi is disabled
+
 #--------#
 
 function change_cpu_governor {
@@ -21,6 +25,27 @@ function change_cpu_governor {
     done
 }
 
+function wifi_toggle {
+    rfkill_id=$(rfkill list | grep "Wireless LAN" | grep -o "^[0-9]")
+    marker="/tmp/lid_root_wifi_toggle"
+    
+    if [ "$1" == "block" ]; then
+        (
+            touch $marker
+            
+            for i in $(eval echo {1..$wifi_control_delay}); do
+                sleep 1
+                [ ! -e $marker ] && exit
+            done
+            
+            rfkill $1 $rfkill_id
+        ) &
+    else
+        [ -e $marker ] && rm $marker
+        rfkill $1 $rfkill_id
+    fi
+}
+
 #--------#
 
 if grep -q "open" /proc/acpi/button/lid/LID/state; then
@@ -36,12 +61,14 @@ while true; do
 			LIDSTATE="OPENED"
 			modprobe hid_multitouch
 			[ "$governor_control" == "true" ] && change_cpu_governor $LIDSTATE
+			[ "$wifi_control" == "true" ] && wifi_toggle unblock
 		fi
 	else
 		if [ $LIDSTATE == "OPENED" ]; then
 			LIDSTATE="CLOSED"
 			modprobe -r hid_multitouch
 			[ "$governor_control" == "true" ] && change_cpu_governor $LIDSTATE
+			[ "$wifi_control" == "true" ] && wifi_toggle block
 		fi
 	fi
 
